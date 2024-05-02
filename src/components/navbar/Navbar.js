5db17429873f10c30c8fdbe13bad040819d6e24a
Shiv Prakash Singh <144921828+shivprakashsinghjii@users.noreply.github.com>
2024-05-02 15:51:53 +0530

    contact,About and home page get completed

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Links from "./links/Links";
 import styles from "./navbar.module.css";
+import Link from "next/link";
 const Navbar = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.logo}>logo</div>
+      <Link href="/" className={styles.logo}>
+        logo
+      </Link>
       <div>
         <Links />
       </div>
